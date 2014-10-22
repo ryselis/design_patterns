3de06cbc423bc015ea76a6b76261ee9956a1b210
ryselis <djryse@gmail.com>
2014-10-22 18:00:26 +0300

    singleton

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Singleton.Singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+    }
+}
---
+++ src/Singleton/Singleton.java
@@ -0,0 +1,19 @@
+package Singleton;
+
+/**
+ * Created by ryselis on 14.10.22.
+ */
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        if (instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}

6a8b690843b4a82463cd83b457e7f5eca15115c9
ryselis <djryse@gmail.com>
2014-10-22 18:02:39 +0300

    singleton

--- src/Singleton/Singleton.java
+++ src/Singleton/Singleton.java
@@ -10,7 +10,7 @@ public class Singleton {
 
     }
 
-    public static Singleton getInstance(){
+    public synchronized static Singleton getInstance(){
         if (instance == null){
             instance = new Singleton();
         }
